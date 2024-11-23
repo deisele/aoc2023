2fba534d370b1782a49fabee850229734e8e7dd8
Daniel Eisele <daniel_eisele@gmx.de>
2024-11-23 22:50:31 +0100

    Add solution for day 22

--- day22/main.cpp
+++ day22/main.cpp
@@ -4,15 +4,24 @@
 #include <QStringList>
 #include <QList>
 #include <QDebug>
+#include <compare>
+#include <algorithm>
 
-void handler(int signal) {
-    Q_UNUSED(signal);
-    Q_ASSERT(0);
-}
+struct Cube {
+    int z, x, y;
+
+    friend constexpr std::strong_ordering operator<=>(Cube a, Cube b) = default;
+};
+
+struct Brick {
+    Cube a, b;
+
+    friend constexpr std::strong_ordering operator<=>(Brick a, Brick b) = default;
+};
+
+QList<Brick> dropBricks(QList<Brick> bricks);
 
 int main() {
-    signal(SIGABRT, &handler);
-
     QTextStream out(stdout);
 
     QFile file("input");
@@ -23,9 +32,72 @@ int main() {
 
     QTextStream in(&file);
 
+    QList<Brick> bricks;
+
     while (!in.atEnd()) {
         QString line = in.readLine();
+
+        QStringList parts = line.split("~");
+
+        QStringList aParts = parts[0].split(",");
+        Cube a = {aParts[2].toInt(), aParts[0].toInt(), aParts[1].toInt()};
+
+        QStringList bParts = parts[1].split(",");
+        Cube b = {bParts[2].toInt(), bParts[0].toInt(), bParts[1].toInt()};
+
+        if (a.z > b.z) {
+            std::swap(a, b);
+        }
+
+        bricks.append({a, b});
     }
 
+    std::sort(bricks.begin(), bricks.end());
+
+    bricks = dropBricks(bricks);
+
+    std::sort(bricks.begin(), bricks.end());
+
+    int safeToRemove = 0;
+    int totalFallenBricks = 0;
+
+    for (int i = 0; i < bricks.size(); i++) {
+        QList<Brick> testPile = bricks;
+        testPile.remove(i);
+
+        QList<Brick> droppedTestPile = dropBricks(testPile);
+
+        int fallenBricks = 0;
+        for (int j = 0; j < testPile.size(); j++) {
+            if (testPile[j] != droppedTestPile[j]) {
+                fallenBricks++;
+            }
+        }
+
+        safeToRemove += (fallenBricks == 0);
+        totalFallenBricks += fallenBricks;
+    }
+
+    out << "Safe to remove: " << safeToRemove << "\n";
+    out << "Total fallen bricks: " << totalFallenBricks << "\n";
+
     return 0;
+}
+
+QList<Brick> dropBricks(QList<Brick> bricks) {
+    for (Brick &brick: bricks) {
+        int minZ = 1;
+
+        for (const Brick &other: bricks) {
+            if (other.a.z < brick.a.z && other.b.x >= brick.a.x && other.a.x <= brick.b.x && other.b.y >= brick.a.y && other.a.y <= brick.b.y) {
+                minZ = std::max(minZ, other.b.z + 1);
+            }
+        }
+
+        int diff = brick.a.z - minZ;
+        brick.a.z -= diff;
+        brick.b.z -= diff;
+    }
+
+    return bricks;
 }
